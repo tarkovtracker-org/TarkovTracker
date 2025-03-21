5cecb7b0e436a1753391e1011a53cb369c52d907
Dys <dysektai@gmail.com>
2025-03-21 04:10:54 -0400

    Update i18n.js to use language code from navigator for improved localization support

--- tarkov-tracker/src/plugins/i18n.js
+++ tarkov-tracker/src/plugins/i18n.js
@@ -2,9 +2,12 @@
 import { createI18n } from "vue-i18n";
 import messages from "@intlify/vite-plugin-vue-i18n/messages";
 
+// Extract just the language code from navigator.language (e.g., 'en' from 'en-US')
+const languageCode = navigator.language.split(/[-_]/)[0];
+
 const i18n = createI18n({
   globalInjection: true,
-  locale: navigator.language,
+  locale: languageCode,
   fallbackLocale: "en",
   messages,
 });
