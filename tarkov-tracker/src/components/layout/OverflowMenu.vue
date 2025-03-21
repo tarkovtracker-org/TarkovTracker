482ca5c2349cf0234caf51e149050b509620a4ff
Dys <dysektai@gmail.com>
2025-03-21 04:07:58 -0400

    Refactor OverflowMenu.vue for improved readability and structure
    - Enhanced template formatting for better clarity
    - Cleaned up button component layout for consistent style
    - Improved computed property formatting for better readability

--- tarkov-tracker/src/components/layout/OverflowMenu.vue
+++ tarkov-tracker/src/components/layout/OverflowMenu.vue
@@ -1,14 +1,33 @@
 <template>
   <v-card min-width="300" class="pa-2">
     <v-container>
-      <v-select v-model="currentLocale" prepend-icon="mdi-translate" density="compact" :items="localeItems"
-        :label="$t('app_bar.overflow_menu.language')" variant="outlined" hide-details></v-select>
-      <v-btn v-if="!userStore.hideAllTips" color="red" prepend-icon="mdi-comment-question-outline" class="mt-4"
-        width="100%" @click="enableHideAllTips">
+      <v-select
+        v-model="currentLocale"
+        prepend-icon="mdi-translate"
+        density="compact"
+        :items="localeItems"
+        :label="$t('app_bar.overflow_menu.language')"
+        variant="outlined"
+        hide-details
+      ></v-select>
+      <v-btn
+        v-if="!userStore.hideAllTips"
+        color="red"
+        prepend-icon="mdi-comment-question-outline"
+        class="mt-4"
+        width="100%"
+        @click="enableHideAllTips"
+      >
         {{ $t("app_bar.overflow_menu.hide_all_tips") }}
       </v-btn>
-      <v-btn v-if="userStore.hiddenTipCount > 0 || userStore.hideAllTips" color="green"
-        prepend-icon="mdi-comment-question-outline" class="mt-4" width="100%" @click="unhideTips">
+      <v-btn
+        v-if="userStore.hiddenTipCount > 0 || userStore.hideAllTips"
+        color="green"
+        prepend-icon="mdi-comment-question-outline"
+        class="mt-4"
+        width="100%"
+        @click="unhideTips"
+      >
         {{ $t("app_bar.overflow_menu.reset_tips") }}
       </v-btn>
     </v-container>
@@ -42,9 +61,11 @@ const localeItems = computed(() => {
 
 const currentLocale = computed({
   get() {
-    return localeItems.value.filter(
-      (localeItem) => localeItem.value == locale.value
-    )[0] || "en";
+    return (
+      localeItems.value.filter(
+        (localeItem) => localeItem.value == locale.value
+      )[0] || "en"
+    );
   },
   // setter
   set(newValue) {
