7f69c2d080df983aa9606b7566e30375d1a646b8
Dys <dysektai@gmail.com>
2025-03-25 01:57:17 -0400

    Remove Firebase plugin from Vite configuration and set default server port to 5173 for improved development setup.

--- tarkov-tracker/vite.config.js
+++ tarkov-tracker/vite.config.js
@@ -4,7 +4,6 @@ import vue from "@vitejs/plugin-vue";
 import eslintPlugin from "vite-plugin-eslint";
 import vueI18n from "@intlify/vite-plugin-vue-i18n";
 import vuetify from "vite-plugin-vuetify";
-import firebasePlugin from "vite-plugin-firebase";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,25 +22,8 @@ export default defineConfig({
       include: path.resolve(__dirname, "./src/locales/**"),
     }),
     vuetify({ autoImport: true }),
-    firebasePlugin({
-      // mandatory firebase project id
-      projectId: "tarkovtracker-next",
-      // project directory, i.e. where firebase.json is (defaults to `config.root`)
-      root: path.resolve("../"),
-      // whether to materialize (write on disk) `.runtimeconfig.json` for functions emulator (defaults to `false`)
-      materializeConfig: false,
-      // emulator targets (defaults to `['hosting', 'functions']`)
-      targets: [
-        "hosting",
-        "functions",
-        "firestore",
-        "database",
-        "auth",
-        "ui",
-        "pubsub",
-      ],
-      // show UI
-      showUI: true,
-    }),
   ],
+  server: {
+    port: 5173, // Default Vite port
+  },
 });
