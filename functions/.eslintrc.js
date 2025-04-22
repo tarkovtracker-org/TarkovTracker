920dc3c0ab5e063ecc1c6129a1adc0dc23a8ba65
DysektAI <contact@dysektai.com>
2025-04-22 16:17:15 -0400

    Initial setup of API v2 with token auth, progress tracking and task management endpoints

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,11 +1,31 @@
 module.exports = {
   root: true,
   env: {
-    es6: true,
+    es2022: true,
     node: true,
   },
-  extends: ["eslint:recommended", "google"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json"],
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   rules: {
     quotes: ["error", "double"],
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "no-console": "off",
+    "require-jsdoc": "off",
+    "valid-jsdoc": "off",
+    "new-cap": "off",
+    "max-len": ["warn", { code: 120 }],
   },
+  ignorePatterns: [
+    "lib/**/*",
+    "node_modules/**/*",
+    ".eslintrc.js",
+    "vitest.config.js",
+  ],
 };
