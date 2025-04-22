0ffd1a728ebe5bcdb2ce0088213ec8012992f46c
DysektAI <contact@dysektai.com>
2025-04-22 16:17:38 -0400

    Initial Vue3 app setup with task tracking routes and components

---
+++ apollo.config.ts
@@ -0,0 +1,15 @@
+export default {
+  client: {
+    service: {
+      name: "My-Graph-9wt0hn",
+      // URL to the GraphQL API
+      url: "https://api.tarkov.dev/graphql",
+    },
+    // Files processed by the extension
+    includes: [
+      "tarkov-tracker/src/**/*.vue",
+      "tarkov-tracker/src/**/*.js",
+      "tarkov-tracker/src/**/*.ts",
+    ],
+  },
+};
