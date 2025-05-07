fc353834235c1d978fc4e929142ca07dae17432f
DysektAI <contact@dysektai.com>
2025-05-07 11:24:37 -0400

    Refactor TeamManagement.vue for improved state handling and code consistency
    - Updated the condition for displaying team members to check for systemStore.$state.team instead of systemStore.userTeam for better clarity.
    - Standardized string quotation marks from double to single for consistency.
    - Enhanced the overall structure of the script section for improved readability and maintainability.

--- frontend/src/pages/TeamManagement.vue
+++ frontend/src/pages/TeamManagement.vue
@@ -7,7 +7,7 @@
       </v-col>
     </v-row>
     <v-row v-if="fireuser.loggedIn" justify="center">
-      <v-col v-if="systemStore.userTeam" cols="12">
+      <v-col v-if="systemStore.$state.team" cols="12">
         <team-members></team-members>
       </v-col>
       <v-col cols="12" sm="12" md="12" lg="6" xl="6">
@@ -20,28 +20,27 @@
   </v-container>
 </template>
 <script setup>
-import { fireuser } from "@/plugins/firebase";
-import { useLiveData } from "@/composables/livedata";
-import { useRoute } from "vue-router";
-import { defineAsyncComponent } from "vue";
-const TeamMembers = defineAsyncComponent(
-  () => import("@/components/teams/TeamMembers.vue"),
-);
-const TeamOptions = defineAsyncComponent(
-  () => import("@/components/teams/TeamOptions.vue"),
-);
-const MyTeam = defineAsyncComponent(
-  () => import("@/components/teams/MyTeam.vue"),
-);
-const TrackerTip = defineAsyncComponent(
-  () => import("@/components/TrackerTip.vue"),
-);
-const TeamInvite = defineAsyncComponent(
-  () => import("@/components/teams/TeamInvite.vue"),
-);
-const { useSystemStore } = useLiveData();
-const systemStore = useSystemStore();
-const route = useRoute();
+  import { fireuser } from '@/plugins/firebase';
+  import { useLiveData } from '@/composables/livedata';
+  import { useRoute } from 'vue-router';
+  import { defineAsyncComponent } from 'vue';
+  const TeamMembers = defineAsyncComponent(
+    () => import('@/components/teams/TeamMembers.vue')
+  );
+  const TeamOptions = defineAsyncComponent(
+    () => import('@/components/teams/TeamOptions.vue')
+  );
+  const MyTeam = defineAsyncComponent(
+    () => import('@/components/teams/MyTeam.vue')
+  );
+  const TrackerTip = defineAsyncComponent(
+    () => import('@/components/TrackerTip.vue')
+  );
+  const TeamInvite = defineAsyncComponent(
+    () => import('@/components/teams/TeamInvite.vue')
+  );
+  const { useSystemStore } = useLiveData();
+  const systemStore = useSystemStore();
+  const route = useRoute();
 </script>
 <style lang="scss" scoped></style>
-
