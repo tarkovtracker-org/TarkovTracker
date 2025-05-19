5a04ccf665e969fe357c94c41ebad293d1799fe9
DysektAI <contact@dysektai.com>
2025-05-19 09:00:05 -0400

    Refactor NeededItemMediumCard.vue for improved readability and consistency
    - Consolidated multiple lines into single lines for better clarity in the template structure.
    - Streamlined conditional rendering and formatting throughout the component to enhance code readability.
    - Removed unnecessary line breaks and improved overall formatting for consistency.

--- frontend/src/components/neededitems/NeededItemMediumCard.vue
+++ frontend/src/components/neededitems/NeededItemMediumCard.vue
@@ -14,10 +14,7 @@
             >
               <template #placeholder>
                 <v-row class="fill-height ma-0" align="center" justify="center">
-                  <v-progress-circular
-                    indeterminate
-                    color="grey-lighten-5"
-                  ></v-progress-circular>
+                  <v-progress-circular indeterminate color="grey-lighten-5"></v-progress-circular>
                 </v-row>
               </template>
             </v-img>
@@ -35,19 +32,12 @@
           <div class="d-flex flex-column align-self-center mt-2 mx-2">
             <template v-if="props.need.needType == 'taskObjective'">
               <task-link :task="relatedTask" />
-              <v-row
-                v-if="lockedBefore > 0"
-                no-gutters
-                class="mb-1 mt-1 d-flex justify-center"
-              >
+              <v-row v-if="lockedBefore > 0" no-gutters class="mb-1 mt-1 d-flex justify-center">
                 <v-col cols="auto" class="mr-1" align="center">
                   <v-icon icon="mdi-lock-open-outline" />
                 </v-col>
                 <v-col cols="auto" align="center">
-                  <i18n-t
-                    keypath="page.tasks.questcard.lockedbefore"
-                    scope="global"
-                  >
+                  <i18n-t keypath="page.tasks.questcard.lockedbefore" scope="global">
                     <template #count>
                       {{ lockedBefore }}
                     </template>
@@ -55,9 +45,7 @@
                 </v-col>
               </v-row>
               <v-row
-                v-if="
-                  levelRequired > 0 && levelRequired > tarkovStore.playerLevel
-                "
+                v-if="levelRequired > 0 && levelRequired > tarkovStore.playerLevel"
                 no-gutters
                 class="mb-1 mt-1 d-flex justify-center"
               >
@@ -76,39 +64,23 @@
             <template v-else-if="props.need.needType == 'hideoutModule'">
               <v-row dense no-gutters class="mb-1 mt-1 d-flex justify-center">
                 <v-col cols="auto" align="center">
-                  <station-link
-                    :station="relatedStation"
-                    class="justify-center"
-                  />
+                  <station-link :station="relatedStation" class="justify-center" />
                 </v-col>
-                <v-col cols="auto" class="ml-1">{{
-                  props.need.hideoutModule.level
-                }}</v-col>
+                <v-col cols="auto" class="ml-1">{{ props.need.hideoutModule.level }}</v-col>
               </v-row>
-              <v-row
-                v-if="lockedBefore > 0"
-                no-gutters
-                class="mb-1 mt-1 d-flex justify-center"
-              >
+              <v-row v-if="lockedBefore > 0" no-gutters class="mb-1 mt-1 d-flex justify-center">
                 <v-col cols="auto" class="mr-1" align="center">
                   <v-icon icon="mdi-lock-open-outline" />
                 </v-col>
                 <v-col cols="auto" align="center">
-                  <i18n-t
-                    keypath="page.tasks.questcard.lockedbefore"
-                    scope="global"
-                  >
+                  <i18n-t keypath="page.tasks.questcard.lockedbefore" scope="global">
                     <template #count>
                       {{ lockedBefore }}
                     </template>
                   </i18n-t>
                 </v-col>
               </v-row>
-              <v-row
-                v-if="levelRequired > 0"
-                no-gutters
-                class="mb-1 mt-1 d-flex justify-center"
-              >
+              <v-row v-if="levelRequired > 0" no-gutters class="mb-1 mt-1 d-flex justify-center">
                 <v-col cols="auto" class="mr-1" align="center">
                   <v-icon icon="mdi-menu-right" />
                 </v-col>
@@ -128,37 +100,22 @@
             class="d-flex fill-height align-self-stretch justify-space-between mt-2 mb-2 mx-2"
           >
             <div class="align-self-end">
-              <v-btn
-                variant="tonal"
-                class="pa-0 ma-0"
-                @click="$emit('decreaseCount')"
+              <v-btn variant="tonal" class="pa-0 ma-0" @click="$emit('decreaseCount')"
                 ><v-icon>mdi-minus-thick</v-icon></v-btn
               >
             </div>
             <div class="mx-1 align-self-end">
-              <v-btn
-                variant="tonal"
-                class="pa-0 px-1 ma-0"
-                @click="$emit('toggleCount')"
-              >
-                {{ currentCount.toLocaleString() }}/{{
-                  neededCount.toLocaleString()
-                }}
+              <v-btn variant="tonal" class="pa-0 px-1 ma-0" @click="$emit('toggleCount')">
+                {{ currentCount.toLocaleString() }}/{{ neededCount.toLocaleString() }}
               </v-btn>
             </div>
             <div class="align-self-end">
-              <v-btn
-                variant="tonal"
-                class="pa-0 ma-0"
-                @click="$emit('increaseCount')"
+              <v-btn variant="tonal" class="pa-0 ma-0" @click="$emit('increaseCount')"
                 ><v-icon>mdi-plus-thick</v-icon></v-btn
               >
             </div>
           </div>
-          <div
-            v-else
-            class="d-flex fill-height align-self-stretch justify-center mt-2 mb-2 mx-2"
-          >
+          <div v-else class="d-flex fill-height align-self-stretch justify-center mt-2 mb-2 mx-2">
             <div class="align-self-end text-center">
               <i18n-t keypath="page.neededitems.neededby" scope="global">
                 <template #users>
@@ -167,12 +124,9 @@
                     :key="userIndex"
                     style="white-space: pre-line"
                   >
-                    <v-icon size="x-small" class="mr-1"
-                      >mdi-account-child-circle</v-icon
+                    <v-icon size="x-small" class="mr-1">mdi-account-child-circle</v-icon
                     >{{ progressStore.getDisplayName(userNeed.user) }}
-                    {{ userNeed.count.toLocaleString() }}/{{
-                      neededCount.toLocaleString()
-                    }}
+                    {{ userNeed.count.toLocaleString() }}/{{ neededCount.toLocaleString() }}
                   </div>
                 </template>
               </i18n-t>
@@ -184,97 +138,92 @@
   </v-lazy>
 </template>
 <script setup>
-import { defineAsyncComponent, computed, inject } from "vue";
-import { useUserStore } from "@/stores/user";
-import { useProgressStore } from "@/stores/progress";
-import { useTarkovData } from "@/composables/tarkovdata";
-import { useTarkovStore } from "@/stores/tarkov";
-const TaskLink = defineAsyncComponent(
-  () => import("@/components/tasks/TaskLink.vue"),
-);
-const StationLink = defineAsyncComponent(
-  () => import("@/components/hideout/StationLink.vue"),
-);
-const props = defineProps({
-  need: {
-    type: Object,
-    required: true,
-  },
-});
-defineEmits(["increaseCount", "decreaseCount", "toggleCount"]);
-const progressStore = useProgressStore();
-const tarkovStore = useTarkovStore();
-useTarkovData();
-const {
-  selfCompletedNeed,
-  relatedTask,
-  relatedStation,
-  lockedBefore,
-  neededCount,
-  currentCount,
-  levelRequired,
-  item,
-  teamNeeds,
-  imageItem,
-} = inject("neededitem");
-const itemImageClasses = computed(() => {
-  return {
-    [`item-bg-${item.value.backgroundColor}`]: true,
-    rounded: true,
-    "elevation-2": true,
-    "item-image": true,
-    "pa-1": true,
-    "fill-height": true,
-  };
-});
-const itemCardClasses = computed(() => {
-  return {
-    "item-complete":
-      selfCompletedNeed.value || currentCount.value >= neededCount.value,
-    "fill-height": true,
-  };
-});
+  import { defineAsyncComponent, computed, inject } from 'vue';
+  import { useUserStore } from '@/stores/user';
+  import { useProgressStore } from '@/stores/progress';
+  import { useTarkovData } from '@/composables/tarkovdata';
+  import { useTarkovStore } from '@/stores/tarkov';
+  const TaskLink = defineAsyncComponent(() => import('@/components/tasks/TaskLink.vue'));
+  const StationLink = defineAsyncComponent(() => import('@/components/hideout/StationLink.vue'));
+  const props = defineProps({
+    need: {
+      type: Object,
+      required: true,
+    },
+  });
+  defineEmits(['increaseCount', 'decreaseCount', 'toggleCount']);
+  const progressStore = useProgressStore();
+  const tarkovStore = useTarkovStore();
+  useTarkovData();
+  const {
+    selfCompletedNeed,
+    relatedTask,
+    relatedStation,
+    lockedBefore,
+    neededCount,
+    currentCount,
+    levelRequired,
+    item,
+    teamNeeds,
+    imageItem,
+  } = inject('neededitem');
+  const itemImageClasses = computed(() => {
+    return {
+      [`item-bg-${item.value.backgroundColor}`]: true,
+      rounded: true,
+      'elevation-2': true,
+      'item-image': true,
+      'pa-1': true,
+      'fill-height': true,
+    };
+  });
+  const itemCardClasses = computed(() => {
+    return {
+      'item-complete': selfCompletedNeed.value || currentCount.value >= neededCount.value,
+      'fill-height': true,
+    };
+  });
 </script>
 <style lang="scss">
-.item-complete {
-  background: linear-gradient(
-    0deg,
-    rgba(var(--v-theme-complete), 1) 0%,
-    rgba(var(--v-theme-surface), 1) 75%
-  ) !important;
-}
-.item-panel {
-  aspect-ratio: 16/9;
-  min-height: 138px;
-}
-.item-image {
-  min-height: 90px;
-}
-.item-bg-violet {
-  background-color: #2c232f;
-}
-.item-bg-grey {
-  background-color: #1e1e1e;
-}
-.item-bg-yellow {
-  background-color: #343421;
-}
-.item-bg-orange {
-  background-color: #261d14;
-}
-.item-bg-green {
-  background-color: #1a2314;
-}
-.item-bg-red {
-  background-color: #38221f;
-}
-.item-bg-default {
-  background-color: #3a3c3b;
-}
-.item-bg-black {
-  background-color: #141614;
-}
-.item-bg-blue {
-  background-color: #202d32;
-}
+  .item-complete {
+    background: linear-gradient(
+      0deg,
+      rgba(var(--v-theme-complete), 1) 0%,
+      rgba(var(--v-theme-surface), 1) 75%
+    ) !important;
+  }
+  .item-panel {
+    aspect-ratio: 16/9;
+    min-height: 138px;
+  }
+  .item-image {
+    min-height: 90px;
+  }
+  .item-bg-violet {
+    background-color: #2c232f;
+  }
+  .item-bg-grey {
+    background-color: #1e1e1e;
+  }
+  .item-bg-yellow {
+    background-color: #343421;
+  }
+  .item-bg-orange {
+    background-color: #261d14;
+  }
+  .item-bg-green {
+    background-color: #1a2314;
+  }
+  .item-bg-red {
+    background-color: #38221f;
+  }
+  .item-bg-default {
+    background-color: #3a3c3b;
+  }
+  .item-bg-black {
+    background-color: #141614;
+  }
+  .item-bg-blue {
+    background-color: #202d32;
+  }
 </style>
